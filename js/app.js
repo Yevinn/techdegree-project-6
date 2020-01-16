cb4091e2342ffe92e2d6c4c02c124836e26b2aaf
Yevinn <kevin@yevinn.com>
2020-01-16 08:48:54 -0700

    Finished up the challange

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
-const qwerty = document.getElementById('#qwerty');
+const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const buttonReset = document.querySelector('.btn__reset');
+const headline = document.querySelector('.title');
+const overlay = buttonReset.parentNode;
 let missed = 0;
 const phrases = ["There is always money in the banana stand", "All the best cowboys have daddy issues", "Live together die alone", "Everybody hates Hugo", "Stranger in a strange land"];
 
-buttonReset.addEventListener('click', () => {
-  const overlay = buttonReset.parentNode;
+buttonReset.addEventListener('click', e => {
   overlay.style.display = 'none';
+  if(e.target.textContent.toLowerCase() === 'play again'){
+    overlay.style.display = 'flex';
+    location.reload(true);
+  }
 });
 
 const getRandomPhraseAsArray = (array) => {
@@ -23,8 +28,6 @@ const addPhraseToDisplay = (array) => {
     let ul = document.querySelector('#phrase ul');
     let li = document.createElement('li');
     li.textContent = array[i];
-
-    console.log(li)
     if(li.textContent !== ' '){
       li.className = 'letter';
     } else{
@@ -34,5 +37,52 @@ const addPhraseToDisplay = (array) => {
   }
 }
  
-
+//add phrase to the page
 addPhraseToDisplay(newPhrase);
+
+const checkLetter = btn => {
+  const li = document.querySelectorAll('.letter');
+  let matched = null;
+
+  for(i = 0; i< li.length; i++){
+    if(btn === li[i].textContent.toLowerCase()){
+      li[i].classList.add('show');
+      matched = true;
+    } 
+  }
+  return matched;
+}
+
+qwerty.addEventListener('click', (e) => {
+  if(e.target.tagName === 'BUTTON'){
+    e.target.classList.add("chosen");
+    e.target.disabled = true;
+    const guess = e.target.textContent;
+    const letterGuess = checkLetter(guess);
+    if (!letterGuess) {
+      missed++;
+      console.log(missed);
+      let li = document.querySelectorAll('img');
+      li[li.length - missed].src = 'images/lostHeart.png';
+    }
+    checkWin();
+  }
+});
+
+const checkWin = () => {
+  const letter = document.querySelectorAll('.letter');
+  const show = document.querySelectorAll('.show');
+  console.log(letter.length + ' Letter ' + show.length + ' show');
+  if (letter.length === show.length){
+    overlay.classList.add('win');
+    headline.textContent = 'Congrats you Won';
+    overlay.style.display = 'flex';
+    buttonReset.textContent = 'Play Again';
+  } else if (missed >= 5){
+    overlay.classList.add('lose');
+    headline.textContent = 'Sorry, You didn\'t win this time';
+    overlay.style.display = 'flex';
+    buttonReset.textContent = 'Play Again';
+  }
+}
+
